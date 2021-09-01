5a061737ab7202c513d4144a456b356df2c09340
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-09-01 18:11:43 +0200

    Add config to send requests

--- config.go
+++ config.go
@@ -10,3 +10,57 @@
 //********************************************************************************************************************//
 
 package gowoocommerce
+
+import (
+	"bytes"
+	"net/http"
+)
+
+// Config is to define config data
+type Config struct {
+	Path, Method string
+	Body         []byte
+}
+
+// Request is to define the request data
+type Request struct {
+	baseUrl, consumerKey, consumerSecret string
+}
+
+// Send is to send a new request
+func (c *Config) Send(r *Request) (*http.Response, error) {
+
+	// Set url
+	url := r.baseUrl + c.Path
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	// Define header
+	request.Header.Set("Content-Type", "application/json")
+
+	// Define url parameter
+	parameter := request.URL.Query()
+
+	parameter.Add("consumer_key", r.consumerKey)
+	parameter.Add("consumer_secret", r.consumerSecret)
+
+	// Set new url
+	request.URL.RawQuery = parameter.Encode()
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
