e652eb02b7e49352670fee492f26eefb425df9d0
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-09-16 18:30:50 +0200

    Add a function to create & update a term

--- product_attribute_terms.go
+++ product_attribute_terms.go
@@ -16,6 +16,14 @@ import (
 	"fmt"
 )
 
+// ProductAttributeTermBody is to structure the request data
+type ProductAttributeTermBody struct {
+	Name        string `json:"name"`
+	Slug        string `json:"slug"`
+	Description string `json:"description"`
+	MenuOrder   int    `json:"menu_order"`
+}
+
 // ProductAttributeTermsReturn is to decode the data
 type ProductAttributeTermsReturn struct {
 	Id          int    `json:"id"`
@@ -55,6 +63,74 @@ func ProductAttributeTerms(attributeId int, r *Request) ([]ProductAttributeTerms
 	err = json.NewDecoder(response.Body).Decode(&decode)
 	if err != nil {
 		return nil, err
+	}
+
+	// Return data
+	return decode, err
+
+}
+
+// CreateProductAttributeTerms is to create a new term
+func CreateProductAttributeTerms(attributeId int, body ProductAttributeTermBody, r *Request) (ProductAttributeTermsReturn, error) {
+
+	// Convert body
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return ProductAttributeTermsReturn{}, err
+	}
+
+	// Set config for new request
+	c := Config{fmt.Sprintf("/wp-json/wc/v3/products/attributes/%d/terms", attributeId), "POST", convert}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return ProductAttributeTermsReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode ProductAttributeTermsReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return ProductAttributeTermsReturn{}, err
+	}
+
+	// Return data
+	return decode, err
+
+}
+
+// UpdateProductAttributeTerms is to update a term
+func UpdateProductAttributeTerms(attributeId, termId int, body ProductAttributeTermBody, r *Request) (ProductAttributeTermsReturn, error) {
+
+	// Convert body
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return ProductAttributeTermsReturn{}, err
+	}
+
+	// Set config for new request
+	c := Config{fmt.Sprintf("/wp-json/wc/v3/products/attributes/%d/terms/%d", attributeId, termId), "PUT", convert}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return ProductAttributeTermsReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode ProductAttributeTermsReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return ProductAttributeTermsReturn{}, err
 	}
 
 	// Return data
