211301643d2ea49aeb0447b2674c9b5ad167445a
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-09-16 17:13:53 +0200

    Add new function to create, update & delete attributes

--- product_attributes.go
+++ product_attributes.go
@@ -11,7 +11,19 @@
 
 package gowoocommerce
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// ProductAttributesBody is to structure the data
+type ProductAttributesBody struct {
+	Name        string `json:"name"`
+	Slug        string `json:"slug"`
+	Type        string `json:"type"`
+	OrderBy     string `json:"order_by"`
+	HasArchives bool   `json:"has_archives"`
+}
 
 // ProductAttributesReturn is to decode the data
 type ProductAttributesReturn struct {
@@ -52,6 +64,102 @@ func ProductAttributes(r *Request) ([]ProductAttributesReturn, error) {
 	err = json.NewDecoder(response.Body).Decode(&decode)
 	if err != nil {
 		return nil, err
+	}
+
+	// Return data
+	return decode, err
+
+}
+
+// CreateProductAttributes is to create a new attribute
+func CreateProductAttributes(body ProductAttributesBody, r *Request) (ProductAttributesReturn, error) {
+
+	// Convert body
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Set config for new request
+	c := Config{"/wp-json/wc/v3/products/attributes/", "POST", convert}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode ProductAttributesReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Return data
+	return decode, err
+
+}
+
+// UpdateProductAttributes is to update a attribute
+func UpdateProductAttributes(id int, body ProductAttributesBody, r *Request) (ProductAttributesReturn, error) {
+
+	// Convert body
+	convert, err := json.Marshal(body)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Set config for new request
+	c := Config{fmt.Sprintf("/wp-json/wc/v3/products/attributes/%d", id), "PUT", convert}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode ProductAttributesReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Return data
+	return decode, err
+
+}
+
+// DeleteProductAttributes is to delete a product attribute
+func DeleteProductAttributes(id int, r *Request) (ProductAttributesReturn, error) {
+
+	// Set config for new request
+	c := Config{fmt.Sprintf("/wp-json/wc/v3/products/attributes/%d", id), "DELETE", nil}
+
+	// Send request
+	response, err := c.Send(r)
+	if err != nil {
+		return ProductAttributesReturn{}, err
+	}
+
+	// Close request
+	defer response.Body.Close()
+
+	// Decode data
+	var decode ProductAttributesReturn
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return ProductAttributesReturn{}, err
 	}
 
 	// Return data
